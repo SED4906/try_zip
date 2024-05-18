aa5eb33c6d69b1d9fe7791e338e7dbefb6be7353
SED4906 <sed4906birdie@gmail.com>
2024-05-18 04:37:53 -0700

    extend Iterator trait itself

--- src/lib.rs
+++ src/lib.rs
@@ -24,54 +24,51 @@ where
     }
 }
 
-impl<A, B> TryZip<A, B> {
-    pub fn try_zip(iter_a: A, iter_b: Option<B>) -> TryZip<A, B> {
-        TryZip { iter_a, iter_b }
+pub trait TryZipExt<A, B>: Iterator {
+    fn try_zip(self, second: Option<B>) -> TryZip<Self, B>
+    where Self: Sized,
+    {
+        TryZip { iter_a: self, iter_b: second }
     }
 }
 
+impl<I: Iterator, J: Iterator> TryZipExt<I, J> for I {}
+
 #[cfg(test)]
 mod tests {
-    use std::ops::Range;
 
     use super::*;
 
+    use std::ops::Range;
+
     #[test]
     fn first_longer() {
-        let iter_a = 0..10;
-        let iter_b = 0..5;
-        println!(
-            "{:?}",
-            TryZip::try_zip(iter_a, Some(iter_b)).collect::<Vec<_>>()
-        )
+        let iter_a = 0u16..10;
+        let iter_b = 0i8..5;
+        let result = iter_a.try_zip(Some(iter_b)).collect::<Vec<_>>();
+        assert_eq!(result, vec![(0,Some(0)), (1,Some(1)), (2,Some(2)), (3,Some(3)), (4,Some(4)), (5,None), (6, None), (7, None), (8,None), (9,None)])
     }
 
     #[test]
     fn second_longer() {
         let iter_a = 0..5;
         let iter_b = 0..10;
-        println!(
-            "{:?}",
-            TryZip::try_zip(iter_a, Some(iter_b)).collect::<Vec<_>>()
-        )
+        let result = iter_a.try_zip(Some(iter_b)).collect::<Vec<_>>();
+        assert_eq!(result, vec![(0,Some(0)), (1,Some(1)), (2,Some(2)), (3,Some(3)), (4,Some(4))])
     }
 
     #[test]
     fn same_length() {
         let iter_a = 0..10;
         let iter_b = 0..10;
-        println!(
-            "{:?}",
-            TryZip::try_zip(iter_a, Some(iter_b)).collect::<Vec<_>>()
-        )
+        let result = iter_a.try_zip(Some(iter_b)).collect::<Vec<_>>();
+        assert_eq!(result, vec![(0,Some(0)), (1,Some(1)), (2,Some(2)), (3,Some(3)), (4,Some(4)), (5,Some(5)), (6, Some(6)), (7, Some(7)), (8,Some(8)), (9,Some(9))])
     }
 
     #[test]
     fn no_second() {
-        let iter_a = 0..10;
-        println!(
-            "{:?}",
-            TryZip::try_zip(iter_a, None::<Range<u8>>).collect::<Vec<_>>()
-        )
+        let iter_a = 0usize..10;
+        let result = iter_a.try_zip(None::<Range<u8>>).collect::<Vec<_>>();
+        assert_eq!(result, vec![(0,None), (1,None), (2,None), (3,None), (4,None), (5,None), (6, None), (7, None), (8,None), (9,None)])
     }
 }
